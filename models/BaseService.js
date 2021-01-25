055c52d9a9c7e70461a8ee406cf9630139b361bc
Nhan Lam Thanh <lamnhanddtm@gmail.com>
2021-01-25 21:16:02 +0700

    'ahihi'

--- models/BaseService.js
+++ models/BaseService.js
@@ -35,33 +35,4 @@ export class BaseService {
         return promise;
     }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-console.log('Minh La User C');
-console.log('Minh La User C');
-console.log('Minh La User C');
-console.log('Minh La User C');
-=======
-console.log("mình là User B bbb ");
->>>>>>> 1943490f9431c4ea169d1fb5bf740e2f3535bfeb
-console.log('Minh La User C Da fix');
-=======
-
-console.log("ahhahaPhuc")
-<<<<<<< HEAD
->>>>>>> 85792b639d26c9790cfd9cf57b3ca4b6f27508dd
-console.log("ahhahaPhucNeDaFix")
-=======
-console.log("gsdf;iuerw9t423n")
-console.log("342245742398532478")
->>>>>>> f5f32e71f09282ec76b6477b47e186394fcecd98
-console.log("ahihii")
-=======
-console.log("mình là User B ");
-console.log("Quân nè mọi người xin chào kimochi");
-console.log("hah âhh hah aha ");
-
-console.log("Chao thang beo ben trai, ");
->>>>>>> 9ccd88e7d7af96ca460b494f9ad3413808ec0fbc
+console.log('bamemay');
